e9970165162ad61bbab1fdbcfa40b7b11d7942e5
Ljaine Hossam Elsayed Abdelsamad <lujaine.hossam25@gmail.com>
2024-12-01 22:52:03 +0200

    created UserProfile, UpdateProfile and ProfileManager

---
+++ src/Backend/UpdateProfile.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.mindrot.jbcrypt.BCrypt;
+
+import javax.swing.*;
+import java.io.IOException;
+
+import java.util.List;
+
+/**
+ *
+ * @author DELL
+ */
+public class UpdateProfile {
+    private static final String USERS_FILE = "databases/users.json";
+
+
+    public void updateProfilePhoto( String profilePhotoPath, String userId) throws IOException {
+
+        ProfileManager profileManager = new ProfileManager();
+        UserProfile user = profileManager.findUserById(userId);
+        if (user != null) {
+            user.setProfilePhotoPath(profilePhotoPath);
+            profileManager.saveUsers(profileManager.getUsers());
+            JOptionPane.showMessageDialog(null, "Profile photo updated successfully.");
+        } else {
+            JOptionPane.showMessageDialog(null, "User not found.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+
+        public void updateCoverPhoto( String coverPhotoPath, String userId) throws IOException {
+
+            ProfileManager profileManager = new ProfileManager();
+            UserProfile user = profileManager.findUserById(userId);
+            if (user != null) {
+                user.setCoverPhotoPath(coverPhotoPath);
+                profileManager.saveUsers(profileManager.getUsers());
+                JOptionPane.showMessageDialog(null, "Cover photo updated successfully.");
+            } else {
+                JOptionPane.showMessageDialog(null, "User not found.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+
+
+
+            public void updateBio( String bio , String userId) throws IOException {
+
+                ProfileManager profileManager = new ProfileManager();
+                UserProfile user = profileManager.findUserById(userId);
+                if (user != null) {
+                    user.setBio(bio);
+                    profileManager.saveUsers(profileManager.getUsers());
+                    JOptionPane.showMessageDialog(null, "Bio updated successfully.");
+                }else {
+                    JOptionPane.showMessageDialog(null, "User not found.", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+                public void updatePassword( String password, String userId) throws IOException {
+
+                    ProfileManager profileManager = new ProfileManager();
+                    UserProfile user = profileManager.findUserById(userId);
+                    if (user != null ) {
+                        user.setHashedPassword(PasswordHashing.hashPassword(password));
+                        profileManager.saveUsers(profileManager.getUsers());
+                        JOptionPane.showMessageDialog(null, "Password updated successfully.");
+                    }else {
+                        JOptionPane.showMessageDialog(null, "User not found.", "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+
+
+
+
+}
---
+++ src/Backend/UserProfile.java
@@ -0,0 +1,58 @@
+package Backend;
+
+public class UserProfile {
+  
+    private String userID;
+    private String profilePhotoPath;
+    private String coverPhotoPath;
+    private String bio;
+    private String hashedPassword;
+
+    public UserProfile() {
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getHashedPassword() {
+        return hashedPassword;
+    }
+
+    public void setHashedPassword(String hashedPassword) {
+        this.hashedPassword = hashedPassword;
+
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getProfilePhotoPath() {
+        return profilePhotoPath;
+    }
+
+    public void setProfilePhotoPath(String profilePhotoPath) {
+        this.profilePhotoPath = profilePhotoPath;
+    }
+
+    public String getCoverPhotoPath() {
+        return coverPhotoPath;
+    }
+
+    public void setCoverPhotoPath(String coverPhotoPath) {
+        this.coverPhotoPath = coverPhotoPath;
+    }
+
+    public String getUserId() {
+        return userID;
+    }
+}
