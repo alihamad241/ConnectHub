0d9ecf580a24f63ba7a369f634b271696b835372
Ali Ahmed Hamad <es-ali.ahmed2027@alexu.edu.eg>
2024-12-01 14:20:08 +0200

    minor changes in the Posts file so that the user is sent as an argument to the posts

--- src/Frontend/Posts.java
+++ src/Frontend/Posts.java
@@ -23,14 +23,15 @@ public class Posts extends javax.swing.JFrame {
 
     private  User user;
     private static final ContentManager contentManager = new ContentManager();
-    private String path;
+    private String path = null;
 
     /**
      * Creates new form NewJFrame
      */
-    public Posts() {
+    public Posts(User user) {
         initComponents();
         this.setLocationRelativeTo(null);
+        this.user = user;
     }
 
     /**
@@ -248,38 +249,6 @@ public class Posts extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Posts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Posts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Posts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Posts.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Posts().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
