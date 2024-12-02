e3d2839587c367001ef4de015c6b783231526f4b
Hanineyasser <es-haneen.yasser2027@alexu.edu.eg>
2024-12-02 22:38:57 +0200

    making the primary design of the newsfeed

--- src/Backend/ContentManager.java
+++ src/Backend/ContentManager.java
@@ -9,6 +9,7 @@ import org.json.JSONObject;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 
 /**
@@ -19,7 +20,6 @@ public class ContentManager {
 
     private ArrayList<Content> contents;
     private static final String CONTENTS_FILE = "databases/content.json";
-    private static final JSONArray contentsArray = DatabaseManager.readJSONFile(CONTENTS_FILE);
 
     public ContentManager() {
         contents = new ArrayList<>();
@@ -32,6 +32,7 @@ public class ContentManager {
 
     public void removeContent(Content content) {
         contents.remove(content);
+        JSONArray contentsArray = DatabaseManager.readJSONFile(CONTENTS_FILE);
         for(int i = 0; i < contentsArray.length(); i++){
             JSONObject contents = contentsArray.getJSONObject(i);
             if(contents.getString("contentId").equals(content.getContentId())){
@@ -42,18 +43,20 @@ public class ContentManager {
         DatabaseManager.writeJSONFile(CONTENTS_FILE, contentsArray);
     }
 
-    public void removeStory(){
-        for(Content content : contents){
-            if(content.getIsStory()){
-                if(content.getTime().isBefore(LocalDateTime.now().minusDays(1))){ //If the story is older than 24 hours it will be removed
-                    removeContent(content);
-                }
+    public void removeStory() {
+        Iterator<Content> iterator = contents.iterator();
+        while (iterator.hasNext()) {
+            Content content = iterator.next();
+            if (content.getIsStory() && content.getTime().isBefore(LocalDateTime.now().minusDays(1))) {
+                iterator.remove();
+                removeContent(content);
             }
         }
     }
 
     public static void writeContent(Content content) {
         JSONObject newContentObject = new JSONObject();
+        JSONArray contentsArray = DatabaseManager.readJSONFile(CONTENTS_FILE);
             newContentObject.put("content", content.getContent());
             newContentObject.put("imagePath", content.getImagePath());
             newContentObject.put("contentId", content.getContentId());
