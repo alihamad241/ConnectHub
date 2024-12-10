d17511e46d808c9b5a518cdda828c175378c06a1
Ljaine Hossam Elsayed Abdelsamad <lujaine.hossam25@gmail.com>
2024-12-11 00:20:31 +0200

    created a Group interface to be implemented by class GroupManagement and ProxyGroup

--- src/Backend/Group.java
+++ src/Backend/Group.java
@@ -7,13 +7,13 @@ public interface Group {
     public void removePendingRequest(User user);
     public void addUser(User user, String role);
     public void removeUser(User user);
-    public void updateRole(User user, String role);
     public void deleteGroup();
     public void promoteUser(User user);
     public void demoteUser(User user);
     public void approveRequest(User user);
     public void rejectRequest(User user);
     public void leaveGroup(User user);
+
 
 
 }
